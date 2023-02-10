6dd0bcf7383fa5fa5566048c42c4368b523d69ec
Shivam-SV <shivam.vastav33@gmail.com>
2023-02-10 21:55:33 +0530

    feat: change project setup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,14 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>📲 Chatify</title>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-    </div>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        @vite('resources/css/app.css')
+    </head>
+    <body>
+        <div id="app"></div>
+
+        @vite('resources/js/app.js')
+    </body>
 </html>
