385369ca27b3522e723344b7829602fb4fbf28da
shivam netweb <102536014+Shivam-netweb@users.noreply.github.com>
2023-09-27 23:23:21 +0530

    setup react

---
+++ resources/js/index.jsx
@@ -0,0 +1,10 @@
+import React, { ReactDOM } from "react";
+
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(
+    <React.StrictMode>
+
+    </React.StrictMode>
+);
