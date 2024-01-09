63fb68f2c0be2ccddd9f1eb7f127b64a1f53e775
Shivam-SV <shivam.vastav33@gmail.com>
2024-01-09 18:06:09 +0530

    feat: floating button

--- resources/js/pages/chats/index.jsx
+++ resources/js/pages/chats/index.jsx
@@ -5,16 +5,18 @@ import ChatsHeader from "../../components/ChatsHeader";
 export default function Chats(){
     return (
         <>
-            <div className="w-100 flex flex-col h-screen border-x border-primary-content">
-                <div className="border-b border-primary-content">
-                    <ChatsHeader />
+            <div className=" relative">
+                <div className="w-100 flex flex-col h-screen border-x border-primary-content">
+                    <div className="border-b border-primary-content">
+                        <ChatsHeader />
+                    </div>
+                    <div className="grow overflow-y-auto">
+                        <ChatContacts />
+                    </div>
                 </div>
-                <div className="grow overflow-y-auto">
-                    <ChatContacts />
+                <div className="floating-buttons sticky right-1 bottom-3 px-6 flex items-end flex-col">
+                    <button className="btn btn-primary btn-sm btn-circle"><i className='bx bx-plus'></i></button>
                 </div>
-                {/* <div className="border-t border-primary-content">
-                    <ChatsFooter />
-                </div> */}
             </div>
         </>
     );
